e335bab5df9b2301ce0140711deda012f1bb97ed
Ian Wu <onlinemad@gmail.com>
2015-12-03 23:38:08 +0800

    Added checkValue() function to calculate checksum value.

--- test/pay2go.test.js
+++ test/pay2go.test.js
@@ -1,5 +1,5 @@
 var assert = require('chai').assert;
-var pay2go = require('../lib/pay2go');
+var pay2go = require('../index');
 
 suite('pay2go', function() {
   suite('constructor', function() {
@@ -13,6 +13,23 @@ suite('pay2go', function() {
     });
     test('should throw Error', function() { 
       assert.throw(function () { new pay2go(); }, Error);
+    });
+  });
+  suite('#checkValue()', function() {
+    test('should generate checkValue', function() { 
+      var _pay2go = new pay2go({
+        merchantID: '123456',
+        hashKey: '1A3S21DAS3D1AS65D1',
+        hashIV: '1AS56D1AS24D'
+      });
+      var _order = {
+        MerchantOrderNo: '20140901001',
+        TimeStamp: '1403243286',
+        Version: '1.1',
+        Amt: 200
+      }
+      assert.equal(_pay2go.checkValue(_order), '841F57D750FB4B04B62DDC3ECDC26F1F4028410927DD28BD5B2E34791CC434D2');
+      // assert.instanceOf(_pay2go, pay2go, '_pay2go is an instance of pay2go');
     });
   });
 });
